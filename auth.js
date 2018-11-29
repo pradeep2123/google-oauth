529b0f3611661460850297d8e8ff158eee491f5c
pradeep2123 <35168431+pradeep2123@users.noreply.github.com>
2018-11-29 16:05:30 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,7 @@
+module.exports={
+    'googleAuth':{
+        'clientID':'68484365934-6741u5haj4jedlku76re9d2e9t5npuep.apps.googleusercontent.com',
+        'clientSecret':'i57wdWm1I95W0gILl-tfrEET',
+        'callbackURL':'http://localhost:2018/auth/google/callback'
+    }
+}
