91628ef6775802bb754daf1d43b90e4465a10318
pradeep2123 <35168431+pradeep2123@users.noreply.github.com>
2018-11-30 18:40:12 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,19 +2,12 @@ module.exports={
     'googleAuth':{
         'clientID':'68484365934-6741u5haj4jedlku76re9d2e9t5npuep.apps.googleusercontent.com',
         'clientSecret':'i57wdWm1I95W0gILl-tfrEET',
-        'callbackURL':'http://localhost:2018/auth/google/callback'
-    },
-
-    
-    'freshAuth':{
-
-    'client_id':      "68484365934-6741u5haj4jedlku76re9d2e9t5npuep.apps.googleusercontent.com",
-    'client_secret':  "i57wdWm1I95W0gILl-tfrEET",
-    'Authorize_url':  "https://accounts.google.com/o/oauth2/auth",
-    'token_url':      "https://www.googleapis.com/oauth2/v3/token",
-    'options':{
-    'scope':        "read"
-    },
-    'token_type':     "account"
-    }
+        'callbackURL':'http://localhost:2018/auth/google/callback',
+        'Authorize_url':  "https://accounts.google.com/o/oauth2/auth",
+        'token_url':      "https://www.googleapis.com/oauth2/v3/token",
+        'options':{
+        'scope':        "read"
+        },
+        'token_type':     "account"
+        }
 }
